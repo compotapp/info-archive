955ef4ecb9d4d3398c9fd83d6e77c9ece2425719
apotapov <alexandrpotapov90@gmail.com>
2024-09-30 16:42:08 +0300

    master start commit

---
+++ src/main/java/com/pot/app/infoarchive/InfoArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.pot.app.infoarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pot/app/infoarchive/InfoArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pot.app.infoarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
